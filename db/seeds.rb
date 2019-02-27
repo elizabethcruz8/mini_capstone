bc0aaccd89e28f344fa2cf9a945019c4d0b5f7cc
elizabethcruz8 <elicruz92.10@gmail.com>
2019-02-26 19:37:18 -0600

    Create all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+controller1 = Product.new({name:"DualShock4_Gold",price:48,image_url:"https://m.media-amazon.com/images/I/81ELkUu9omL._AC_UL436_.jpg",description:"Enhanced analog sticks"})
+
+controller2 = Product.new({name:"DualShock4_Crystal",price:55,image_url:"https://m.media-amazon.com/images/I/71uZID+EhLL._AC_UL436_.jpg",description:"Charge efficiently"})
+
+controller3 = Product.new({name:"DualShock4_ChangingColor",price:75,image_url:"https://m.media-amazon.com/images/I/91OaXCM75KL._AC_UL436_.jpg",description:"Color changing effect"})
